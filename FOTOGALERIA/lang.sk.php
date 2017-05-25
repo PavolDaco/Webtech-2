452068a9ac3eae408f0c89784a1e0507b3121981
MatusCirip <matuscirip@gmail.com>
2017-05-25 11:33:52 +0200

    Add files via upload

---
+++ FOTOGALERIA/lang.sk.php
@@ -0,0 +1,35 @@
+﻿<?php
+/* 
+-----------------
+Language: Slovak
+-----------------
+*/
+header ('Content-type: text/html; charset=utf-8');
+$lang = array();
+
+
+
+
+//photos
+$lang['PHOTO_DATE'] = 'Dátum';
+$lang['PHOTO_TITLESK'] = 'Názov (SK)';
+$lang['PHOTO_TITLEEN'] = 'Názov (EN)';
+$lang['PHOTO_CHOOSE'] = 'Vyber obrázky: (max 50, max 2MB)';
+$lang['PHOTO_UPLOAD'] = 'Nahraj obrázky';
+$lang['PHOTO_GALERY'] = 'Zvoľ galériu:';
+$lang['PHOTO_ERROR'] = 'Súbor sa nepodarilo nahrať';
+
+//Video
+$lang['VIDEO_ADDR'] = 'Link na video';
+$lang['VIDEO_ADD'] = 'Pridaj video';
+$lang['VIDEO_UPLOAD'] = 'Nahraj video';
+$lang['VIDEO_ERR_EXST'] = 'Súbor s týmto náyvom už existuje';
+$lang['VIDEO_ERR_BIG'] = 'Súbor je príliš veľký';
+$lang['VIDEO_ERR_BAD'] = 'Súbor má nesprávny formát';
+$lang['VIDEO_UP_OK'] = 'Upload bol ukončený';
+$lang['VIDEO_ERR_NO'] = 'Chyba číslo ';
+
+
+
+
+?>
