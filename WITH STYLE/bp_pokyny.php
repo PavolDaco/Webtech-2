95954b088286c77aa67b3a6f08a7365d97d88cff
xkoplingerm <xkoplingerm@stuba.sk>
2017-05-26 19:51:29 +0200

    Add files via upload

---
+++ WITH STYLE/bp_pokyny.php
@@ -0,0 +1,94 @@
+<?php
+include 'includes/header.php';
+
+?>
+<style>
+    h4{
+        margin-top: 30px;
+    }
+</style>
+<title>BP pokyny</title>
+<div class="container">
+	
+	<h1>BP pokyny</h1>
+	<h4><strong>Ukončovanie predmetov BP1, BP2, BZP</strong></h4>
+    <h4>Bakalársky projekt 1</h4>
+	<div>
+		<table class='table table-hover' >
+			<tr>
+				<td>Zodpovedný: </td>
+				<td>doc. Ing. Vladimír Kutiš, PhD.</td>
+			</tr>
+			<tr>
+				<td>Hodnotenie predmetu:</td>
+				<td>klasifikovaný zápočet</td>
+			</tr>
+			<tr>
+				<td>Štandardný čas plnenia: </td>
+				<td>3. roč. bakalárskeho štúdia, zimný semester</td>
+			</tr>
+			<tr>
+				<td colspan="2">Pre získanie klasifikovaného zápočtu musí študent odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.januára daného roku. Prácu na projekte hodnotí vedúci práce.</td>
+				
+			</tr>
+		</table>
+	</div>
+
+    <h4>Bakalársky projekt 2</h4>
+	<div >
+		<table class='table table-hover' >
+			<tr>
+				<td>Zodpovedný: </td>
+				<td>doc. Ing. Vladimír Kutiš, PhD.</td>
+			</tr>
+			<tr>
+				<td>Hodnotenie predmetu:</td>
+				<td>klasifikovaný zápočet</td>
+			</tr>
+			<tr>
+				<td>Štandardný čas plnenia: </td>
+				<td>3. roč. bakalárskeho štúdia, letný semester</td>
+			</tr>
+			<tr>
+				<td colspan="2">Pre získanie klasifikovaného zápočtu musí študent do dátumu špecifikovanom v harmonograme štúdia FEI STU odovzdať diplomovú prácu:<br>
+				<ol>
+					<li>v elektronickej forme do AIS</li>
+					<li>v tlačenej forme v počte 2 kusy Ing. Sedlárovi? (A803)</li>
+				</ol>
+				alebo odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.júna daného roku. 
+				</td>
+				
+			</tr>
+		</table>
+	</div>
+
+	Prácu na projekte hodnotí vedúci práce.
+	<div>
+        <h4>Bakalárska záverečná práca</h4>
+		<table class='table table-hover' >
+			<tr>
+				<td>Zodpovedný: </td>
+				<td>doc. Ing. Vladimír Kutiš, PhD.</td>
+			</tr>
+			<tr>
+				<td>Hodnotenie predmetu:</td>
+				<td>klasifikovaný zápočet</td>
+			</tr>
+			<tr>
+				<td>Štandardný čas plnenia: </td>
+				<td>3. roč. bakalárskeho štúdia, letný semester</td>
+			</tr>
+			<tr>
+				<td colspan="2">Pre získanie skúšky musí študent obhájiť tému svojej diplomovej práce pred štátnicovou komisiou, ktorá zároveň udeľuje známku za obhajobu.</td>
+				
+			</tr>
+		</table>
+	</div>
+
+</div>
+
+
+<?php
+include 'includes/footer.php';
+
+?>
