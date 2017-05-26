d85e3aed2bfd37ddf77b84aea8bfbe1044d8ed9a
Pavol Dačo <palki15.daco@gmail.com>
2017-05-26 12:02:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,44 @@
 <?php
 include 'includes/header.php';
-
 ?>
 <title>Home Page</title>
+<style type="text/css" media="screen">
+  body { 
+    background: url('includes/1.jpg') no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+  .box{
+    text-align: center;   
+    background-color: black;
+  }
+  .quote{
+    font-family: "Brush Script MT", cursive;
+    size: 20px;
+    color: white;
+    font-size: 3em;
+  }
+  .text{
+   font-size: 4em;
+   color: white;
+   font-family: Impact, Haettenschweiler, "Franklin Gothic Bold", Charcoal, "Helvetica Inserat", "Bitstream Vera Sans Bold", "Arial Black", sans serif;
+ }
+ .row{
+  padding-top: 15%;  
+ }
+</style>
 
 <div class="container">
-
-<h1>Domov</h1>
-	
-
-
+  <div class="row">
+    <div class="box">
+     <h1 class="text">
+      Ústav automobilovej mechatroniky FEI STU
+    </h1>   
+    <p class="quote">„Neučíme sa pre školu, ale pre život.“ Seneca</p>
+  </div>
+</div>
 
 </div>
 
@@ -17,5 +46,4 @@ include 'includes/header.php';
 
 <?php
 include 'includes/footer.php';
-
 ?>
